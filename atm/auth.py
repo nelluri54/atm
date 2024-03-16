a9a11d114be6a59788b867f353f811c6900f450c
nelluri54 <156047879+nelluri54@users.noreply.github.com>
2024-03-16 15:37:57 +0530

    Delete atm directory

--- atm/auth.py
+++
@@ -1,9 +0,0 @@
-class authent:
-    def autho(self,username,password):
-        self.username=username
-        self.password=password
-        if self.username==self.password:
-            return True
-        else:
-            return False
-    
